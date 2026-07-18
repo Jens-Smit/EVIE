a431b21fae8685c7e11ec8976d4fe4b4c70ed0b7
Jens Smit <164412746+Jens-Smit@users.noreply.github.com>
2026-07-18 13:43:36 +0200

    feat: Add UserProfileRepository class

---
+++ src/Repository/UserProfileRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\UserProfile;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<UserProfile>
+ */
+class UserProfileRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, UserProfile::class);
+    }
+
+    public function save(UserProfile $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(UserProfile $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
