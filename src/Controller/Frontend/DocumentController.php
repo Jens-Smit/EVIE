8a4113004fbceba6ae9d08f529105f364753b8e5
Jens Smit <164412746+Jens-Smit@users.noreply.github.com>
2026-08-11 08:27:52 +0200

    Add Frontend DocumentController for rendering document templates

---
+++ src/Controller/Frontend/DocumentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Frontend;
+
+use App\Repository\DocumentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DocumentController extends AbstractController
+{
+    #[Route('/documents', name: 'app_documents')]
+    public function index(DocumentRepository $documentRepository): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $documents = $documentRepository->findByUser($user->getId());
+
+        return $this->render('documents/index.html.twig', [
+            'documents' => $documents
+        ]);
+    }
+}
