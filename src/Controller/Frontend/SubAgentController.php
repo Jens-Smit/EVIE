66b0aaae76c000bfb7f3525c52eb1674d260079d
Jens Smit <164412746+Jens-Smit@users.noreply.github.com>
2026-08-11 08:28:03 +0200

    Add Frontend SubAgentController for rendering sub-agent templates

---
+++ src/Controller/Frontend/SubAgentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Frontend;
+
+use App\Repository\SubAgentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SubAgentController extends AbstractController
+{
+    #[Route('/subagents', name: 'app_subagents')]
+    public function index(SubAgentRepository $subAgentRepository): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $subAgents = $subAgentRepository->findByUser($user->getId());
+
+        return $this->render('subagents/index.html.twig', [
+            'subAgents' => $subAgents
+        ]);
+    }
+}
