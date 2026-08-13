62ca8fed45e1015883fcb252481b5c50b4a80734
Jens Smit <164412746+Jens-Smit@users.noreply.github.com>
2026-08-13 20:59:37 +0200

    Fix SecurityController.php: correct namespace and use statements

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,12 +2,9 @@
 
 namespace AppController;
 
-use AppEntityUser;
-use AppRepositoryUserRepository;
 use SymfonyBundleFrameworkBundleControllerAbstractController;
 use SymfonyComponentHttpFoundationRequest;
 use SymfonyComponentHttpFoundationResponse;
-use SymfonyComponentPasswordHasherHasherUserPasswordHasherInterface;
 use SymfonyComponentRoutingAnnotationRoute;
 use SymfonyComponentSecurityHttpAuthenticationAuthenticationUtils;
 
@@ -17,7 +14,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('app_dashboard');
+            return $this->redirectToRoute('app_home');
         }
 
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -32,48 +29,6 @@ class SecurityController extends AbstractController
     #[Route('/logout', name: 'app_logout')]
     public function logout(): void
     {
-        // Symfony handles logout automatically
         throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
-
-    #[Route('/register', name: 'app_register', methods: ['GET', 'POST'])]
-    public function register(
-        Request $request,
-        UserRepository $userRepository,
-        UserPasswordHasherInterface $passwordHasher
-    ): Response {
-        if ($request->isMethod('POST')) {
-            $email = $request->request->get('email');
-            $password = $request->request->get('password');
-            $firstName = $request->request->get('first_name');
-            $lastName = $request->request->get('last_name');
-
-            // Prüfe ob User bereits existiert
-            if ($userRepository->findByEmail($email)) {
-                $this->addFlash('error', 'Ein User mit dieser E-Mail existiert bereits.');
-                return $this->redirectToRoute('app_register');
-            }
-
-            // Erstelle neuen User
-            $user = new User();
-            $user->setEmail($email);
-            $user->setPassword($passwordHasher->hash($password));
-            $user->setFirstName($firstName);
-            $user->setLastName($lastName);
-            $user->addRole('ROLE_USER');
-
-            $userRepository->save($user, true);
-
-            $this->addFlash('success', 'Registrierung erfolgreich! Bitte melden Sie sich an.');
-            return $this->redirectToRoute('app_login');
-        }
-
-        return $this->render('security/register.html.twig');
-    }
-
-    #[Route('/dashboard', name: 'app_dashboard')]
-    public function dashboard(): Response
-    {
-        return $this->render('dashboard/index.html.twig');
     }
 }
