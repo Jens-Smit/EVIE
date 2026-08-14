3e747d04a4c4d68fd8501fb7db5cf73ce33e0d5e
Jens Smit <j.smit@hotmail.de>
2026-08-14 07:18:41 +0200

    buggy

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\AI\AiBundle\AiBundle::class => ['all' => true],
+    App\Mcp\EvieMcpBundle::class => ['all' => true],
 ];
 
